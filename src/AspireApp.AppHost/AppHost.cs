6492b5af3082735a95b79c3c44bf49190e2559fe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-09-26 18:36:16 +0530

    feat(minimal-api): add MinimalApi project with WeatherForecast controller and configuration

--- src/AspireApp.AppHost/AppHost.cs
+++ src/AspireApp.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AspireApp_BlazorWeb>("aspireapp-blazorweb");
 
+builder.AddProject<Projects.AspireApp_MinimalApi>("aspireapp-minimalapi");
+
 await builder.Build().RunAsync().ConfigureAwait(false);
---
+++ src/AspireApp.MinimalApi/Program.cs
@@ -0,0 +1,29 @@
+using AspireApp.ServiceDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync().ConfigureAwait(false);
